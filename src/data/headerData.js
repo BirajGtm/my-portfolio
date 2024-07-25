60ee3b2e854b079ae3cd55502d9a2b130d4d63cc
Biraj Gautam <birajgtm@gmail.com>
2024-07-24 20:10:40 -0400

    Resume from drive

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -1,4 +1,4 @@
-import resume from '../assets/pdf/resume.pdf'
+// import resume from '../assets/pdf/resume.pdf'
 import profileImg from "../assets/profile.png"
 
 export const headerData = {
@@ -6,5 +6,5 @@ export const headerData = {
     title: "Web Developer/IT Professional",
     desciption: "In the digital realm, where code is the language and innovation the currency, I thrive as a web/mobile developer and IT specialist. My journey through software landscapes has honed my ability to create intuitive web solutions and deliver robust IT services. With a strong foundation in JavaScript and Kotlin, I craft dynamic web experiences and tackle technical challenges, always ready to adapt and excel in the ever-evolving tech ecosystem.",
     image: profileImg,
-    resumePdf: resume
+    resumePdf: "https://drive.google.com/file/d/1McgRQURMNMkU2Y4AzAyslDlv4zhKpz9t/view?usp=sharing"
 }
