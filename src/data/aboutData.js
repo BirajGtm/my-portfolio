6ee4ecee5c4156d2f91b4514faa6b72cce736c5a
Biraj Gautam <birajgtm@gmail.com>
2024-07-30 20:26:12 -0400

    updated bio

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,7 +1,7 @@
 export const aboutData = {
     title: "Who I am",
     description1: "My name is Biraj.",
-    description2: "I'm a web developer and IT professional based in Toronto. I specialize in creating dynamic web applications and providing IT solutions. In my free time, I love diving into new technologies and frameworks to enhance my skills and explore innovative ways to solve problems. I enjoy working on freelance projects and developing my own tech solutions, always striving to expand my knowledge and stay at the forefront of the ever-evolving tech landscape.",
+    description2: "I'm a web/mobile developer and IT professional based in Toronto. I specialize in creating dynamic applications for mobile/web and providing IT solutions. In my free time, I love diving into new technologies and frameworks to enhance my skills and explore innovative ways to solve problems. I enjoy working on freelance projects and developing my own tech solutions, always striving to expand my knowledge and stay at the forefront of the ever-evolving tech landscape.",
     image: 2,
     resume: ""
 }
