13d1e4d4ecb4aaf4dae683c7fe7dbe266dca9d7a
Biraj Gautam <birajgtm@gmail.com>
2025-05-05 16:59:09 -0400

    Fix

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,7 +1,7 @@
 export const aboutData = {
     title: "Who I am",
     description1: "My name is Biraj.",
-    description2: "My name is Biraj. I’m a web/mobile developer, IT professional, and machine learning enthusiast based in Toronto. I specialize in building dynamic, user-centered applications and delivering robust IT solutions. In my free time, I explore new technologies, frameworks, and AI techniques to sharpen my skills and craft innovative solutions. Whether working on freelance projects or developing my own ideas, I’m always driven to learn, create, and push the boundaries of what’s possible.",
+    description2: "I’m a web/mobile developer, IT professional, and machine learning enthusiast based in Toronto. I specialize in building dynamic, user-centered applications and delivering robust IT solutions. In my free time, I explore new technologies, frameworks, and AI techniques to sharpen my skills and craft innovative solutions. Whether working on freelance projects or developing my own ideas, I’m always driven to learn, create, and push the boundaries of what’s possible.",
     image: 2,
     resume: ""
 }
